a18a7fa6aa75fee19d3073cb94d05ff021138ce0
succhi111 <suchendramishra396@gmail.com>
2022-08-19 19:41:59 +0530

    program to reverse any number

---
+++ src/ReverseNumber.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ReverseNumber {
+
+    public static void main(String[] args) {
+        int n,r;
+        System.out.println("Enter any number");
+
+        Scanner ref=new Scanner(System.in);
+        n=ref.nextInt();
+
+        while (n>0) {
+            r = n % 10;
+            System.out.println(r);
+            n = n / 10;
+        }
+    }
+}
